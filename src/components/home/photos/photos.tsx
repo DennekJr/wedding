6bad69af4faaf943b2f81f0a7d0a08fd70a46e8d
stanjhaei <ceejay.orji@theitsolutions.io>
2020-10-15 12:59:28 +0200

    Cloudinary support

--- src/components/home/photos/photos.tsx
+++ src/components/home/photos/photos.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./photos.styles.css";
+import { Image, Transformation, CloudinaryContext } from "cloudinary-react";
 
 export const Photos = () => {
-  // const classes = useStyles();
+  const images = [
+    "image1_jmaznx",
+    "image2_ww0fyv",
+    "image3_zgrnuf",
+    "image4_cmjsln",
+    "image5_wndujb",
+    "image6_uutjce",
+    "image8_vps76g",
+    "image9_imm4qh",
+    "image10_fmbj8q",
+    "image11_lcbmzh",
+    "image12_ezreen",
+    "image14_tgn2xq",
+    "image13_be1fsj",
+  ];
   return (
     <div className="photosContainer">
       <h2>Photos</h2>
@@ -11,52 +26,23 @@ export const Photos = () => {
         <span>.</span>
         <hr />
       </div>
-      <div className="imageGrid">
-        <div className="gridRow">
-          <div>
-            <img src="/images/image1.jpg" alt="first" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image2.jpg"} alt="second" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image3.jpg"} alt="Third" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image4.jpg"} alt="fourth" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image5.jpg"} alt="fifth"width={400} height={600} />
-          </div>
-          <div>
-            <img src={"/images/image6.jpg"} alt="Sixth" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image8.jpg"} alt="eight" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image9.jpg"} alt="ninth" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image10.jpg"} alt="tenth" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image15.jpg"} alt="fifteenth" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image11.jpg"} alt="eleventh" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image12.jpg"} alt="twelfth" width={400} height={600}/>
-          </div>
-          <div>
-            <img src={"/images/image14.jpg"} alt="fourteenth" width={400} height={500}/>
-          </div>
-          <div>
-            <img src={"/images/image13.jpg"} alt="thirteenth" width={400} height={500}/>
+      <CloudinaryContext cloudName="stanjhae">
+        <div className="imageGrid">
+          <div className="gridRow">
+            {images.map((image) => (
+              <div>
+                <Image publicId={image}>
+                  <Transformation
+                    width="1000"
+                    gravity="faces"
+                    crop="fill"
+                  />
+                </Image>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
+      </CloudinaryContext>
     </div>
   );
 };

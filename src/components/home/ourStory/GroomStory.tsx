a081db81be5c5dd11002642887f773358c070efa
stanjhaei <ceejay.orji@theitsolutions.io>
2020-10-21 19:37:14 +0200

    Edited groom story

--- src/components/home/ourStory/GroomStory.tsx
+++ src/components/home/ourStory/GroomStory.tsx
@@ -16,35 +16,37 @@ export const GroomStory = () => {
 
         <p className="ourStoryParagraph">
           Your love came to me as a blessing in disguise at a time when I was so
-          lucky to realize this, if it were in 2020 then I would say your love
-          wore a mask which made it a bit hard recognize but guess what, love
-          conquerors all. Faith brought Kuchy and I together I would say through
-          a mutual friend which at the time was passing through some
-          difficulties but God uses some situations to lead us to our fortune
-          and that is you my sunrise.
+          lucky to realize this. If it were in 2020, I would say your love wore
+          a mask which made it a bit hard recognize but guess what, love
+          conquerors all! Faith brought Kuchy and I together through a mutual
+          friend which at the time was passing through some difficulties but God
+          used some situations to lead us to our fortune and that is you my
+          sunrise.
           <br />
           <br />
           Our time together in school felt like family away from home as I
-          remember you remained a shoulder for me to cry on and comforter at my
-          difficult moments especially during my final exams and Project (Wink),
-          the best moments will never fade away as we grew in love at heart,
-          then came the question which struck hard “What are we?” am sorry If I
-          ever took what we shared for granted, you are everything to me and I
-          never seemed to know how best to answer this question when it came out
-          but I could manage to ask if you would be my woman. <br />
+          remember you remained a shoulder for me to cry on and a comforter in
+          my difficult moments especially during my final exams and project
+          😉. The best moments will never fade away as we grew in love at
+          heart. Then came the question which struck hard “What are we?” I'm
+          sorry If I ever took what we shared for granted. You are everything to
+          me and I never seemed to know how best to answer this question when it
+          came but I could manage to ask if you would be my woman. <br />
           <br />
           You also could not answer at the blink of it but your eyes wrote an
-          epistle of a future for us, your actions with love, honestly, faith,
-          consistently builds us, at times when I could not even imagine and my
-          Heart and lips would only say “there goes my baby!” We have had our
-          fair share of arguments, disagreements but we still wax even stronger
-          and to me it’s a strong sign of our love as we strive to be better
-          partners every other day. <br />
+          epistle of a future for us. Your actions with love, honestly and faith
+          consistently builds us even at times I could not even imagine. My
+          heart and lips would only say “there goes my baby!”. We have had our
+          fair share of arguments and disagreements but we still wax even
+          stronger and to me it’s a strong sign of our love as we strive to be
+          better partners every other day. <br />
           <br />I never got a befitting way or opportunity to ask you to be my
-          wife, am sorry I found a straight way to your heart through your
-          sumptuous plate of food (Wink Emoji) and thank you for accepting to
-          deal with my flaws. I promise to love, Cherish and respect you forever
-          and a day more!! <br />
+          wife. I'm sorry I found a straight way to your heart through your
+          sumptuous plate of food 😉. <br />
+          <br />
+          Thank you for accepting to deal with my flaws. <br />
+          <br />I promise to love, Cherish and respect you forever and a day
+          more!! <br />
           <br />I LOVE YOU MY SUNRISE (MY SHAN PEPE)
         </p>
       </div>
